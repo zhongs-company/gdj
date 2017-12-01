a30165cb6ef8ff2f7d83158a6f43093c51aa7afc
zhongs <191492580@qq.com>
2017-12-01 23:42:14 +0800

    删除了svn文件，打算后面从新上传代码到svn

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,28 +3,28 @@
         123
     </div>
 </template>
-
 <script>
 import { mapState } from "vuex";
 export default {
-  computed: mapState({
-    tabs: state => state.yfdt.tabs
-  }),
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
-  },
-  created() {
-    this.$store.dispatch("getSysType", ()=>{
-        console.log(this.tabs)
-    });
-  },
-  methods: {}
+    computed: mapState({
+        tabs: state => state.yfdt.tabs
+    }),
+    data() {
+        return {
+            msg: "Welcome to Your Vue.js App"
+        };
+    },
+    created() {
+        this.$store.dispatch("getSysType", () => {
+            console.log(this.tabs)
+        });
+    },
+    methods: {}
 };
-</script>
 
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+
 
 </style>
--- src/pages/test.vue
+++ src/pages/test.vue
@@ -3,21 +3,19 @@
         {{msg}}
     </div>
 </template>
-
 <script>
-    export default {
-        name: "HelloWorld",
-        data() {
-            return {
-                beforeHash: "",
-                currentHash: "",
-                msg: "Welcome to Your Vue.js App"
-            };
-        }
-    };
+export default {
+    name: "HelloWorld",
+    data() {
+        return {
+            beforeHash: "",
+            currentHash: "",
+            msg: "Welcome to Your Vue.js App"
+        };
+    }
+};
 
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
 
